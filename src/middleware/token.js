f0f208b499475134919a5c4d60449f24293b84b5
dharmatin <dharmatin@gmail.com>
2018-03-07 15:30:37 +0700

    [DENI]: Remove Unnecessary paramters

--- src/middleware/token.js
+++ src/middleware/token.js
@@ -4,7 +4,7 @@ import { unAuthorizedResponse } from '../libs/responseHandler';
 const REDIS_DB = 1;
 const KEY_PREFIX = 'oauth_access_tokens';
 
-export const UserInfo = ((req, res, next) => {
+export const UserInfo = (() => {
   const getUserInfo = async(token) => {
     const {client: redisClient} = new Redis(REDIS_DB);
     redisClient.selectAsync(REDIS_DB);
