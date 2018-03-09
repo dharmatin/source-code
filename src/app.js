8501cdc7c0012dcdf40f7b9250105b333c66220e
Banu Araidi <banu.araidi@iproperty.com>
2018-03-09 17:26:39 +0700

    activate check token

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('*', setTranslator);
-// app.use('*', setUserInfoToken);
+app.use('*', setUserInfoToken);
 
 web.register(app, baseController);
 web.register(app, listingController);
