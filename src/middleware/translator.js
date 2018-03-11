d8e2c9b2854488d790dcd48bc397a38e25b4de77
Banu Araidi <banu.araidi@iproperty.com>
2018-03-11 09:26:45 +0700

    set lang

--- src/middleware/translator.js
+++ src/middleware/translator.js
@@ -4,6 +4,7 @@ import userInfoTokenService from '../services/userInfoTokenService';
 export const setTranslator = async (req: any, res: any, next: any) => {
   const lang = req.acceptsLanguages('en', 'id');
   const translator = require(`../config/locales/${lang}.json`);
+  req.lang = lang;
   req.app.set('translator', translator);
   next();
 };
