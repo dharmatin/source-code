59715412561b2d3537d9703903a3a4c438ab1308
dharmatin <dharmatin@gmail.com>
2018-03-12 10:08:29 +0700

    [IB-1731] deni: fix related error redis search

--- src/dao/userInfoToken/index.js
+++ src/dao/userInfoToken/index.js
@@ -2,11 +2,11 @@
 import Redis from '../../libs/connections/RedisClient';
 
 export default {
-  searchUserByToken: async (token: string): Object => {
+  searchUserByToken: async(token: string): Object => {
     const REDIS_DB = 1;
     const KEY_PREFIX = 'oauth_access_tokens';
     const { client: redisClient } = new Redis(REDIS_DB);
-    const result = redisClient.getAsync(`${KEY_PREFIX}:${token}`);
+    const result = await redisClient.getAsync(`${KEY_PREFIX}:${token}`);
     redisClient.quit();
 
     return JSON.parse(result);
--- src/middleware/token.js
+++ src/middleware/token.js
@@ -3,10 +3,10 @@ import userInfoTokenService from '../services/userInfoTokenService';
 import { handlerUnauthorized } from '../libs/responseHandler';
 import _ from 'lodash';
 
-export const setUserInfoToken = async (req: any, res: any, next: any): any => {
-  const token = !_.isNil(req.get('Authorization'))
-    ? req.get('Authorization')
-    : '';
+export const setUserInfoToken = async(req: any, res: any, next: any): any => {
+  const token = !_.isNil(req.get('Authorization')) ?
+    req.get('Authorization') :
+    '';
   const userInfo = await userInfoTokenService.getUserInfoToken(token);
   if (!_.isEmpty(token) && _.isEmpty(userInfo)) {
     handlerUnauthorized(res);
