b1146b60918d0b75dcf07bbfa32f2be0930232c4
dharmatin <dharmatin@gmail.com>
2018-03-12 11:50:48 +0700

    [IB-1731] deni: Add unit test for formatting the userInfoToken

---
+++ tests/unit/userInfoFormatter.spec.js
@@ -0,0 +1,23 @@
+// @flow
+import chai from 'chai';
+import userInfo from '../fixture/userInfo';
+import { formatterUserInfoToken } from '../../src/services/formatters/userInfoTokenFormatter';
+import type{ UserInfoToken } from '../../src/services/formatters/userInfoTokenFormatter/types';
+
+declare var describe: any;
+declare var it: any;
+const { expect } = chai;
+
+describe('User Info Token Formatter', () => {
+  it('Should return an object of UserInfoToken', () => {
+    const format: UserInfoToken = formatterUserInfoToken(userInfo);
+    expect(format).is.an('object').have.property('userGroup').is.a('string');
+    expect(format).is.an('object').have.property('userID').is.a('string');
+    expect(format).is.an('object').have.property('accessToken').is.a('string');
+  });
+
+  it('Should return an empty object of UserInfoToken, if userInfo object is null', () => {
+    const format: UserInfoToken = formatterUserInfoToken(null);
+    expect(format).is.an('object').that.is.empty;
+  });
+});
