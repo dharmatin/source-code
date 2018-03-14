81aaab09f1da5a6e38384c3ec2b912e5a09d3b53
dharmatin <dharmatin@gmail.com>
2018-03-14 15:10:18 +0700

    [IB-17151] deni: unit test for email queue service

---
+++ tests/unit/emailQueueService.spec.js
@@ -0,0 +1,48 @@
+// @flow
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import EmailQueueDao from '../../src/dao/emailQueue';
+import type { EmailQueue } from '../../src/dao/emailQueue/type';
+import { EmailQueueService } from '../../src/services/emailQueueService';
+
+declare var describe: any;
+declare var it: any;
+declare var afterEach: any;
+
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+const { expect } = chai;
+const sandbox = sinon.createSandbox();
+
+describe('Email Queue services', () => {
+  afterEach(() => {
+    sandbox.restore();
+  });
+  it('Should be return true if the data success save to database', async(): any => {
+    const emailQueueService = new EmailQueueService(EmailQueueDao);
+    const result = await emailQueueService
+      .to('dharmatin@gmail.com')
+      .from('Rumah123.com')
+      .subject('Referral Success')
+      .template('/test')
+      .jsonData({ucok: 'baba'})
+      .save();
+    expect(result).that.is.true;
+  });
+
+  it('Should be return false if the data failed save to database', async(): any => {
+    sandbox.stub(EmailQueueDao, 'save').callsFake((): boolean => false);
+    const emailQueueService = new EmailQueueService(EmailQueueDao);
+    const result = await emailQueueService
+      .to('dharmatin@gmail.com')
+      .from('Rumah123.com')
+      .subject('Referral Success')
+      .template('/test')
+      .jsonData({ucok: 'baba'})
+      .save();
+    expect(result).that.is.false;
+  });
+});
