b54abda6bf0015244201307e0a24d908eed3e462
dharmatin <dharmatin@gmail.com>
2018-03-14 15:09:37 +0700

    [IB-1751] deni: email queue service, use this service using method chainnig for saving data to db

---
+++ src/services/emailQueueService.js
@@ -0,0 +1,78 @@
+// @flow
+import Sequelize from 'sequelize';
+import emailQueueDao from '../dao/emailQueue';
+import type { EmailQueue } from '../dao/emailQueue/type';
+
+export class EmailQueueService {
+  emailQueueDao: Object;
+  emailFrom: string;
+  emailTo: string;
+  emailSubject: string;
+  emailJsonData: Object;
+  emailTemplate: string;
+  emailQueue: Object | EmailQueue;
+
+  constructor(objectDao: Object) {
+    this.emailQueue = {};
+    this.emailFrom = '';
+    this.emailTo = '';
+    this.emailSubject = '';
+    this.emailJsonData = {};
+    this.emailTemplate = '';
+    this.emailQueueDao = objectDao;
+  }
+
+  from(from: string): Object {
+    this.emailFrom = from;
+    return this;
+  }
+
+  to(to: string): Object {
+    this.emailTo = to;
+    return this;
+  }
+
+  subject(subject: string): Object {
+    this.emailSubject = subject;
+    return this;
+  }
+
+  jsonData(data: Object): Object {
+    this.emailJsonData = data;
+    return this;
+  }
+
+  template(src: string): Object {
+    this.emailTemplate = src;
+    return this;
+  }
+
+  async save(): Object {
+    this.setEmailQueue(this);
+    const result = await this.emailQueueDao.save(this.getEmailQueue());
+
+    return result;
+  }
+
+  getEmailQueue(): EmailQueue {
+    return this.emailQueue;
+  }
+
+  setEmailQueue(self: Object) {
+    this.emailQueue.queueId = Sequelize.DEFAULT;
+    this.emailQueue.subject = self.emailSubject;
+    this.emailQueue.from = self.emailFrom;
+    this.emailQueue.to = self.emailTo;
+    this.emailQueue.cc = '';
+    this.emailQueue.bcc = '';
+    this.emailQueue.data = JSON.stringify(self.emailJsonData);
+    this.emailQueue.srcTemplate = self.emailTemplate;
+    this.emailQueue.body = '';
+    this.emailQueue.status = -1;
+    this.emailQueue.createdDate = Sequelize.fn('NOW', 3);
+    this.emailQueue.sendDate = Sequelize.DEFAULT;
+    this.emailQueue.sentDate = Sequelize.DEFAULT;
+  }
+}
+
+export default new EmailQueueService(emailQueueDao);
