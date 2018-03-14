8ab61ea0a2f2da6b8fd87e9e9d5cbb124255a392
dharmatin <dharmatin@gmail.com>
2018-03-14 15:08:48 +0700

    [IB-1751] deni: email queue dao and type

---
+++ src/dao/emailQueue/index.js
@@ -0,0 +1,78 @@
+// @flow
+import Sequelize from 'sequelize';
+import _ from 'lodash';
+import MysqlClient from '../../libs/connections/MysqlClient';
+import type { EmailQueue } from './type';
+
+const DATABASE_NAME = 'default';
+const { client: DBClient } = new MysqlClient(DATABASE_NAME);
+
+class EmailQueueDao {
+  emailQueueModel: Object;
+  EmailQueue: EmailQueue;
+  constructor() {
+    this.emailQueueModel = DBClient.define('email_queue', {
+      queueId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        field: 'queue_id',
+      },
+      subject: {
+        type: Sequelize.STRING(50),
+        field: 'subject',
+      },
+      from: {
+        type: Sequelize.STRING(50),
+        field: 'from',
+      },
+      to: {
+        type: Sequelize.STRING(50),
+        field: 'to',
+      },
+      cc: {
+        type: Sequelize.STRING(50),
+        field: 'cc',
+      },
+      bcc: {
+        type: Sequelize.STRING(50),
+        field: 'bcc',
+      },
+      data: {
+        type: Sequelize.STRING(250),
+        field: 'data',
+      },
+      srcTemplate: {
+        type: Sequelize.STRING(50),
+        field: 'src_template',
+      },
+      body: {
+        type: Sequelize.TEXT,
+        field: 'body',
+      },
+      status: {
+        type: Sequelize.TINYINT(4),
+        field: 'status',
+      },
+      createdDate: {
+        type: Sequelize.DATE,
+        field: 'created_date',
+      },
+      sendDate: {
+        type: Sequelize.DATE,
+        field: 'send_date',
+      },
+      sentDate: {
+        type: Sequelize.DATE,
+        field: 'sent_date',
+      },
+    });
+  }
+
+  async save(value: EmailQueue): Promise<boolean> {
+    const result = await this.emailQueueModel.create(value);
+    return !_.isEmpty(result);
+  }
+}
+
+export default new EmailQueueDao();
---
+++ src/dao/emailQueue/type.js
@@ -0,0 +1,16 @@
+// @flow
+export type EmailQueue = {
+  queueId: number,
+  subject: string,
+  from: string,
+  to: string,
+  cc: string,
+  bcc: string,
+  data: string,
+  srcTemplate: string,
+  body: string,
+  status: number,
+  createdDate: Date,
+  sendDate: Date,
+  sentDate: Date
+};
