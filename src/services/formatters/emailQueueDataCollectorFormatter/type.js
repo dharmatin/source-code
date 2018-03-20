9d134c28894186764373265b73dd00522329224b
dharmatin <dharmatin@gmail.com>
2018-03-20 12:59:23 +0700

    [IB-1758] deni: email queued data formatter

---
+++ src/services/formatters/emailQueueDataCollectorFormatter/type.js
@@ -0,0 +1,32 @@
+// @flow
+
+export type Lister = {
+  name: string,
+  firstName: string,
+  profilePage: string,
+  membership: string,
+  photoUrl: string,
+  email: string
+};
+
+export type Project = {
+  name: string,
+  srcImage: string,
+  priceRange: string,
+  provinceName: string,
+  cityName: string,
+  districtName: string,
+  urlReferral: string,
+  subUnitType: string,
+  builtUp: string,
+  landArea: string
+};
+
+export type Organisation = {
+  name: string,
+  urlPage: string,
+  phoneNumber: string,
+  whatsappNumber: string,
+  email: string,
+  logo: string
+};
