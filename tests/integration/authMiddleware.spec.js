d6598721eeffd1469679580a5b21c21d3f600a57
ihdi_iproperty <ihdinawan.setiawan@iproperty.com>
2018-04-02 15:47:58 +0700

    fix unit test

--- tests/integration/authMiddleware.spec.js
+++ tests/integration/authMiddleware.spec.js
@@ -6,7 +6,7 @@ import userInfo from '../fixture/userInfo.json';
 
 chai.use(chaiHttp);
 const requester = chai.request(app);
-const {expect} = chai;
+const { expect } = chai;
 
 describe('Authentication Middleware', () => {
   let server;
@@ -20,18 +20,20 @@ describe('Authentication Middleware', () => {
   });
 
   describe('GET /listing/v1/listings/:id', () => {
-    it('Should return status code 401 if Authorization header is not valid', (done) => {
+    it('Should return status code 401 if Authorization header is not valid', done => {
       requester
         .get('/listing/v1/listings/nps499')
         .set('Authorization', '123')
         .end((err, res) => {
           expect(err).have.status(401);
           expect(res.body).have.property('error');
-          expect(res.body.error).have.property('code').eql(401);
+          expect(res.body.error)
+            .have.property('code')
+            .eql(401);
           done();
         });
     });
-    it('Should return status code 200 if Authorization header is valid', (done) => {
+    it('Should return status code 200 if Authorization header is valid', done => {
       const token = userInfo.access_token;
       requester
         .get('/listing/v1/listings/nps499')
@@ -42,14 +44,14 @@ describe('Authentication Middleware', () => {
           done();
         });
     });
-    it('Should return status code 200 if Authorization header is not set', (done) => {
-      requester
-        .get('/listing/v1/listings/nps499')
-        .end((err, res) => {
-          expect(err).to.be.a('null');
-          expect(res).have.status(200);
-          done();
-        });
+    it('Should return status code 200 if Authorization header is not set', done => {
+      requester.get('/listing/v1/listings/nps499').end((err, res) => {
+        console.log(err);
+        console.log(res);
+        expect(err).to.be.a('null');
+        expect(res).have.status(200);
+        done();
+      });
     });
   });
 });
--- tests/unit/emailQueueService.spec.js
+++ tests/unit/emailQueueService.spec.js
@@ -1,10 +1,10 @@
 // @flow
+/* eslint-disable no-unused-expressions */
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import EmailQueueDao from '../../src/dao/emailQueue';
-import type { EmailQueue } from '../../src/dao/emailQueue/type';
 import { EmailQueueService } from '../../src/services/emailQueueService';
 
 declare var describe: any;
@@ -28,9 +28,9 @@ describe('Email Queue services', () => {
       .from('Rumah123.com')
       .subject('Referral Success')
       .template('/test')
-      .jsonData({ucok: 'baba'})
+      .jsonData({ ucok: 'baba' })
       .save();
-    expect(result).that.is.true;
+    expect(result).to.be.true;
   });
 
   it('Should be return false if the data failed save to database', async(): any => {
@@ -41,8 +41,8 @@ describe('Email Queue services', () => {
       .from('Rumah123.com')
       .subject('Referral Success')
       .template('/test')
-      .jsonData({ucok: 'baba'})
+      .jsonData({ ucok: 'baba' })
       .save();
-    expect(result).that.is.false;
+    expect(result).to.be.false;
   });
 });
