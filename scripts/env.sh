7540d257a343570c3a86bb304e5313a36451e421
nalin.gupta <nalin.gupta@iproperty.com>
2018-04-03 16:35:52 +0800

    NO need of branch filter

--- scripts/env.sh
+++ scripts/env.sh
@@ -7,7 +7,6 @@ echo '--- :house_with_garden: Check the environment here'
 export ACCOUNT_ID=726150208279
 export BUILD_NUMBER=${BUILDKITE_BUILD_NUMBER:=snapshot}
 export BUILD_COMMIT=${BUILDKITE_COMMIT:=snapshot}
-export ENV_VAR_BRANCH=${BUILDKITE_BRANCH:develop}
 export ECR_REPO_NAME="newlaunch-api-id"
 export DEPLOYER_ECR_URL="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/deployer:latest"
 
