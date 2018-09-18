c2536d7f18d9e2fe01f247e5ecda295154c6dec0
Gilang Al Khansa <gilang.alkhansa@iproperty.com>
2018-09-18 11:31:26 +0700

    [IB-2049] Gilang: unit test - remove base dependcy

--- tests/unit/featuredVideoService.spec.js
+++ tests/unit/featuredVideoService.spec.js
@@ -1,24 +1,16 @@
-import chai from 'chai';
-import sinon from 'sinon';
-import chaiAsPromised from 'chai-as-promised';
-import sinonChai from 'sinon-chai';
+/* eslint-disable */
 import videos from '../fixture/homePageVideoRedis';
 import FeaturedVideoDao from '../../src/dao/featuredVideo';
 import { FeaturedVideoService } from '../../src/services/featuredVideoService';
 import constants from '../../src/config/constants';
 
-chai.use(chaiAsPromised);
-chai.use(sinonChai);
-const { expect } = chai;
-const sandbox = sinon.createSandbox();
-
-describe('Featured Videos', () => {
+describe.only('Featured Videos', () => {
   afterEach(() => {
     sandbox.restore();
   });
 
   context('#getVideosList', () => {
-    it('Should be return list of videos from Redis', async() => {
+    it('Should be return list of videos from Redis', async () => {
       const baseReponse = videos;
       const newChannel = constants.NEWLAUNCH.CHANNELS;
       sandbox.stub(FeaturedVideoDao, 'getVideos').callsFake(() => baseReponse);
